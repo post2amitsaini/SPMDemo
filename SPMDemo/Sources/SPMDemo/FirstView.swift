618145801a64d6d8793d16d4f3815d6d72628bc9
Amit <post2amitsaini@gmail.com>
2022-11-03 19:50:45 +0530

    View added in Package

---
+++ SPMDemo/Sources/SPMDemo/FirstView.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Amit Saini on 03/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct FirstView: View {
+    var body: some View {
+        Text("View from package")
+    }
+}
