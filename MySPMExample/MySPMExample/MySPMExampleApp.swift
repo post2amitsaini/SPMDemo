6c4b62cf4d68c8e0414f37401d658c473eab4178
Amit <post2amitsaini@gmail.com>
2022-11-03 19:55:58 +0530

    SPM example initial code added

---
+++ MySPMExample/MySPMExample/MySPMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySPMExampleApp.swift
+//  MySPMExample
+//
+//  Created by Amit Saini on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MySPMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
